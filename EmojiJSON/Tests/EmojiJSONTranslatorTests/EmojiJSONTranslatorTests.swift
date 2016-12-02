34aa104f7e8a7282cdf3ac679fce1c117beb85a8
Paul Wood III <paul.wood.iii@gmail.com>
2016-12-02 10:13:25 -0500

    learned that I can’t test the existance of files
    Maybe I’ve found a sandbox rule

--- EmojiJSON/Tests/EmojiJSONTranslatorTests/EmojiJSONTranslatorTests.swift
+++ EmojiJSON/Tests/EmojiJSONTranslatorTests/EmojiJSONTranslatorTests.swift
@@ -2,17 +2,40 @@ import XCTest
 @testable import EmojiJSONTranslator
 
 class EmojiJSONTranslatorTests: XCTestCase {
-    func testExample() {
+    func testStaticsExist() {
+        //Given / When the package is loaded into memory by the OS
         
+        //Then
         XCTAssertEqual(Emoji.allEmojis.count, 1284)
         XCTAssertNotNil(Emoji.json)
-        print(Emoji.json)
+        
+    }
+    
+    func testPersistance () {
+        let helper = FileSaver()
+
+        //Given
+        //let fixture = "Tests/EmojiJSONTranslatorTests/TestDictJSON.json"
+        let fixture = "Sources/EmojiJSONTranslator/emojis.json"
+        
+        do {
+            let dict = try?
+                helper.loadJSONDictionary(filename: fixture)
+            // do something with data
+            // if the call fails, the catch block is executed
+            XCTAssertNotNil(dict)
+            print(dict)
+        } catch {
+            XCTFail("Should not throw an error when loading " + error.localizedDescription)
+        }
+        
     }
 
 
     static var allTests : [(String, (EmojiJSONTranslatorTests) -> () throws -> Void)] {
         return [
-            ("testExample", testExample),
+            ("testStaticsExist", testStaticsExist ),
+             ("testPersistance", testPersistance),
         ]
     }
 }
